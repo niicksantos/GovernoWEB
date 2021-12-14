ed5fb58b531445dedc6ce6c692f2d1c6c3706262
gereiz <georgie.reis@outlook.com>
2021-12-14 20:43:19 -0300

    Update cadastrar_noticia.blade.php

--- resources/views/Admin/noticias/cadastrar_noticia.blade.php
+++ resources/views/Admin/noticias/cadastrar_noticia.blade.php
@@ -60,11 +60,13 @@
     </div>
 </div>
 
-<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-<script src="/vendor/unisharp/laravel-ckeditor/adapters/jquery.js"></script>
 <script>
-    $('textarea').ckeditor();
-    // $('.textarea').ckeditor(); // if class is prefered.
+ 
+ $(document).ready(function() {
+    
+    
+
+});
 </script>
 
 @endsection
