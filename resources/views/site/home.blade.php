9ca3b7b0af60ca2c432a3695942746730b6e466c
unknown <georgie.reis@outlook.com>
2022-01-02 22:04:01 -0300

    Ajustes css mobile
    Ajustes css mobile

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -19,11 +19,11 @@
         @endforeach        
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span class="visually-hidden">Anterior</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span class="visually-hidden">Próximo</span>
         </button>
     </div>
 </div>
@@ -89,7 +89,6 @@
                                 </div>
                             </div>                            
                         @endif
-                         
                     @endforeach  
                 </div>
             </div>
