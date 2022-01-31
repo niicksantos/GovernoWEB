ce9a82e30dc7da9a1ed499a2ef9c59c0ff408c00
unknown <georgie.reis@outlook.com>
2022-01-31 10:19:54 -0300

    .

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -56,11 +56,12 @@
     <div class="row">
         <div class="col-12s">
             <div class="acess_rap_btns">
-                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-1.png') }}" alt=""><p>E-SIC</p></div></a>
-                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-2.png') }}" alt=""><P>AGENDA DE REUNIÕES</P></div></a>
-                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>TRANSPARÈNCIA</p></div></a>
-                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>CONTAS PUBLICAS/LRF</p></div></a>
-                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-4.png') }}" alt=""><p>DÚVIDAS FREQUENTES</p></div>                </a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>REQUERIMENTO</p></div></a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><P>PORTARIA</P></div></a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>DECRETO</p></div></a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>AGENDA DE REUNIÕES</p></div></a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>CONTAS PÚBLICAS</p></div></a>
+                <a href="#"><div class="btn_acrpd"><img src="{{ url('img/icon-3.png') }}" alt=""><p>DÚVIDAS FREQUENTES</p></div></a>
             </div>
         </div>
     </div>
