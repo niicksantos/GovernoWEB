34caab0afebb43bf45862ba441091844ff5d5c63
unknown <georgie.reis@outlook.com>
2022-01-31 11:14:37 -0300

    .

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -68,12 +68,65 @@
 </div>
 
 
+<!----- NOTICIAS ----->
 <div class="container">
     <div class="row">
         <div class="col-12">
             <div class="noticias_titulo">
                 <div class="losango"></div>
                 <p>ÚLTIMAS NOTÍCIAS</p>
+                <div class="losango"></div>
+            </div>
+        </div>
+    </div>
+    <div class="div_noticias">
+        <div class="row">
+            <div class="col-12">      
+                    <div class="row">
+                    @foreach ($destaque as $dest)
+                        <div class="col-12 noticia_destaque">
+                            <a href="{{ route('noticias.noticia', ['id' => $dest->id])}}" ><img class="img_noticia_dest" src="{{ $dest->capa }}" alt="{{ $dest->titulo }}">
+                            <!--<p>{!! $dest->titulo !!}</p> -->
+                        
+                            <div class="texto_noticia">
+                            {!! Str::limit($dest->texto, 600) !!}
+                            </a>
+                            </div>
+                        </div>
+                        <div class="calendario_noticia_dest">
+                            {{$dest->data}}   
+                        </div>
+                    @endforeach
+                <div class="box_noticias">            
+                    @foreach ($noticia as $not)
+                        @if ($not->destaque == 0)
+                            <div class="row not_linha">
+                                <div class="col-3 noticia_padrao">
+                                    <a href="{{ route('noticias.noticia', ['id' => $not->id])}}" ><img class="img_noticia" src="{{ $not->capa }}" alt="{{ $not->titulo }}">
+                                    <p>{!! $not->titulo !!}</p></a> 
+                                </div>
+                            </div>                            
+                        @endif
+                        <div class="calendario_noticia">
+                            @php
+                                echo date('d-m-Y', strtotime($not->data));
+                            @endphp   
+                        </div>
+                    @endforeach  
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
+
+
+<!----- VIDEOS ----->
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="noticias_titulo">
+                <div class="losango"></div>
+                <p>GALERIA DE VÍDEOS</p>
                 <div class="losango"></div>
             </div>
         </div>
@@ -109,7 +162,6 @@
         </div>    
     </div>
 </div>
-
 
 @endsection
 
